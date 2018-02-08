5f70b3b2cc1bbf524c0654dbd48018ff4cfb5c07
nadia@mumuki.org <nadia@mumuki.org>
2018-02-08 12:01:36 -0500

    Mumuki Export on 2018-02-08 12:01:36 -0500

--- 00018_Filtrados/test..js
+++ 00018_Filtrados/test..js
@@ -1,20 +1,20 @@
 describe("", function() {
-  it("saldosDosMesesComLucro([1]) es [1]", function() {
+  it("saldosDosMesesComLucro([1]) é [1]", function() {
     assert.deepEqual(saldosDosMesesComLucro([1]), [1])
   })
-  it("saldosDosMesesComLucro([2, 20, 20]) es [2, 20, 20]", function() {
+  it("saldosDosMesesComLucro([2, 20, 20]) é [2, 20, 20]", function() {
     assert.deepEqual(saldosDosMesesComLucro([2, 20, 20]), [2, 20, 20])
   })
-  it("saldosDosMesesComLucro([]) es []", function() {
+  it("saldosDosMesesComLucro([]) é []", function() {
     assert.deepEqual(saldosDosMesesComLucro([]), [])
   })
-  it("saldosDosMesesComLucro([0, 0, 0]) es []", function() {
+  it("saldosDosMesesComLucro([0, 0, 0]) é []", function() {
     assert.deepEqual(saldosDosMesesComLucro([0, 0, 0]), [])
   })
-  it("saldosDosMesesComLucro([10, -10, 2, 100]) es [10, 2, 100]", function() {
+  it("saldosDosMesesComLucro([10, -10, 2, 100]) é [10, 2, 100]", function() {
     assert.deepEqual(saldosDosMesesComLucro([10, -10, 2, 100]), [10, 2, 100])
   })
-  it("saldosDosMesesComLucro([-1, -2, -3]) es []", function() {
+  it("saldosDosMesesComLucro([-1, -2, -3]) é []", function() {
     assert.deepEqual(saldosDosMesesComLucro([-1, -2, -3]), [])
   })
 })
